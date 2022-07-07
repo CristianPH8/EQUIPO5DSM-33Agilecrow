e0e7755883622ca9c62d962f9cbd34639ad5d039
PerezHurtadCristianRaymundoDSM-33 <al222110790@gmail.com>
2022-07-06 22:58:05 -0500

    Nevos cambios

---
+++ Pagina/Iniciarsesion.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- importamos archivos css -->
+
+<link rel="stylesheet" href="./estilos/Footers.css">
+<link rel="stylesheet" href="./estilos/encabezado.css">
+<link rel="stylesheet" href="./estilos/paginaprincipal.css">
+<script src="https://kit.fontawesome.com/7a66a4eab9.js" crossorigin="anonymous"></script>
+<!-- importamos bootstrap -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">  
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+        crossorigin="anonymous"></script>
+<link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/sign-in/">
+
+
+</head>
+
+<body class="bg-success  text-dark bg-opacity-25">
+  <!-- Header  -->
+  <div id="menuContainer">
+
+  </div>
+<!-- Lo del medio body  -->
+<div  id="inicioContainer"></div>
+
+
+<!-- Footer -->
+<footer>
+<div id="footerContainer">
+  
+  </div>
+</footer>
+
+<!-- End Footer -->
+
+
+
+
+</body>
+<!-- Importamos ajax query -->
+<script src="./JavaScript/jquery-3.6.0.min.js"></script>
+
+<!-- importamos layoutjs -->
+<script src="./JavaScript/footer.js"></script>
+
+<!-- Aqui importamos nuestros archivos js -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"
+    integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js"
+    integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy"
+    crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+ 
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  
+<!-- <script src="javascript.js"></script> -->
+
+</html>
---
+++ Pagina/JavaScript/footer.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#menuContainer').load('./html-recortados/header1.html');
+    $('#principalContainer').load('./html-recortados/principal.html');
+    $('#footerContainer').load('./html-recortados/footer.html');
+    $('#inicioContainer').load('./html-recortados/inicio.html');
+
+
+
+  });
